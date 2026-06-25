8cde6ccf5212652ed7ee48fb902eded27f94ef91
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-06-25 10:45:45 -0500

    Created Chores controller and manager

---
+++ client/src/components/chores/ChoresList.jsx
@@ -0,0 +1,24 @@
+import { Table } from "reactstrap"
+
+export const ChoresList = () => {
+
+    return (
+        <>
+            <h2>Chores</h2>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Chore</th>
+                        <th>Frequency</th>
+                        <th>Difficulty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        
+                    }
+                </tbody>
+            </Table>
+        </>
+    )
+}
